cae753fec279e4579faee279fedb37b0a6873a4a
Med Marwen Meddeb <marwen.meddeb1812@gmail.com>
2023-06-06 09:44:13 +0200

    Updates
    Add player position to multiplayer mode
    Assign an automatic spawn point to the player when generating a game in Lobby

--- Assets/Scripts/GameLobbyManager.cs
+++ Assets/Scripts/GameLobbyManager.cs
@@ -1,9 +1,19 @@
 ﻿using Assets.Scripts.NetworkClasses;
 using Photon.Realtime;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+[System.Serializable]
+public class SpawnPointData
+{
+    public float x;
+    public float y;
+    public float z;
+    public Color color;
+}
 
 public class GameLobbyManager : MonoBehaviour
 {
@@ -23,6 +33,14 @@ public class GameLobbyManager : MonoBehaviour
     private NetworkingGameManager roomController;
 
     public string selectedGameMode;
+
+    // Spawn point data
+    public List<SpawnPointData> spawnPoints = new List<SpawnPointData>();
+
+    public List<Image> PlayerColorList = new List<Image>();
+
+    public List<Color> AvailableColors = new List<Color>();
+
 
     // Start is called before the first frame update
     void Start()
@@ -74,12 +92,29 @@ public class GameLobbyManager : MonoBehaviour
 
     public void OpenLobby()
     {
-        //change the UI
+        // Change the UI
         CancelCreateRoomButtonObj.SetActive(true);
         PlayMultiplayerButtonObj.SetActive(false);
 
-        //connect to the server
+        // Connect to the server
         gameLauncher.Connect();
+
+        // Assign colors to image components
+        for (int i = 0; i < PlayerColorList.Count; i++)
+        {
+            // Check if there are available colors to assign
+            if (i < AvailableColors.Count)
+            {
+                // Assign the color to the image component
+                PlayerColorList[i].color = AvailableColors[i];
+                PlayerColorList[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                // If there are no more available colors, deactivate the image component
+                PlayerColorList[i].gameObject.SetActive(false);
+            }
+        }
     }
 
     public void CancelRoomCreation()
@@ -123,16 +158,31 @@ public class GameLobbyManager : MonoBehaviour
 
         Debug.Log("Trouvé " + Players.Count + " Joueur(s)");
 
-        //assign all the players to places on the screen
+        int index = 0;
         foreach (int id in Players.Keys)
         {
-            PlayerNameTextList[id - 1].text = Players[id].NickName;
+            PlayerNameTextList[index].text = Players[id].NickName;
+
+            // Set the color for the image component
+            Color assignedColor = GetAssignedColor(Players[id]);
+            PlayerColorList[index].color = assignedColor;
+
+            index++;
         }
 
-        if (Players.Count >= 8)
-        { 
+
+        if (Players.Count >= 2)
+        {
             EnoughPlayersAreIn();
         }
+    }
+
+    private Color GetAssignedColor(Player player)
+    {
+        // Implement your logic for assigning colors to players
+        // This example assigns colors based on the player's actor number
+        int colorIndex = player.ActorNumber % AvailableColors.Count;
+        return AvailableColors[colorIndex];
     }
 
     private void EnoughPlayersAreIn()
